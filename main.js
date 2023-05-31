ec91e5ddc046e9f725af036f9aa04acdf6d21062
Parwinder Singh <pinta.e86@gmail.com>
2023-05-31 15:08:18 -0400

    changed main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.onload = function() {
     var paragraph = document.querySelector('p');
     paragraph.textContent = 'This text was changed by JavaScript!';
   };
-  
+  console.log('Hello');
   // Example code to handle a button click event
   var button = document.querySelector('button');
   button.addEventListener('click', function() {
