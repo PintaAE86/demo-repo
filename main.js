35b7130b6e7c198690c57e85d51782eb5acba676
Parwinder Singh <pinta.e86@gmail.com>
2023-05-31 14:48:35 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// JavaScript code goes here
+
+// Example code to change the text of the paragraph when the page loads
+window.onload = function() {
+    var paragraph = document.querySelector('p');
+    paragraph.textContent = 'This text was changed by JavaScript!';
+  };
+  
+  // Example code to handle a button click event
+  var button = document.querySelector('button');
+  button.addEventListener('click', function() {
+    alert('Button clicked!');
+  });
+  
