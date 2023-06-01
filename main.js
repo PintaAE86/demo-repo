c2854e28c06ba2d0388bff018455147fc2d59368
Parwinder Singh <pinta.e86@gmail.com>
2023-06-01 13:00:23 -0400

    Second commint

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 // JavaScript code goes here
 
 // Example code to change the text of the paragraph when the page loads
-window.onload = function() {
-    var paragraph = document.querySelector('p');
-    paragraph.textContent = 'This text was changed by JavaScript!';
-  };
-  console.log('Hello');
-  // Example code to handle a button click event
-  var button = document.querySelector('button');
-  button.addEventListener('click', function() {
-    alert('Button clicked!');
-  });
-  
+
+console.log("What you want")
